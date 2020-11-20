eef3337d20f826d70f25616abf3aaa521f5a3330
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2020-11-21 00:22:33 +0200

    about section and css structure

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//pie
+    var ctxP = document.getElementById("pieChart").getContext('2d');
+    var myPieChart = new Chart(ctxP, {
+      type: 'pie',
+      data: {
+        labels: ["digital Transformer","Web developer"],
+        datasets: [{
+          data: [80,120],
+          backgroundColor: ["#ad5389","#3c1053"],
+          hoverBackgroundColor: ["#D8BFD8", "#EE82EE"]
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
